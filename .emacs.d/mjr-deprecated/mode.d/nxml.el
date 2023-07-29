55cbb618fd2af4cf7fc2977b2016a799a7cde6f9
Matthew Ryall <matthew@mexico86.co.uk>
2023-07-29 08:22:16 +0100

    Remove old config

--- .emacs.d/mjr-deprecated/mode.d/nxml.el
+++
@@ -1,5 +0,0 @@
-
-(setq rng-schema-locating-files '("schemas.xml"
-                                  "~/.schemas/schemas.xml"
-                                  "/usr/share/emacs/23.3/etc/schema/schemas.xml"
-                                  ))
